0f44471b14e366f3bedf52a15cb0e62881b00c34
AllandoAlmeida <alandoalmeida0803@7j50z3.onmicrosoft.com>
2023-10-18 17:21:03 -0300

    feat: add footer components

---
+++ src/components/ui/footer.tsx
@@ -0,0 +1,10 @@
+export const Footer = () => {
+  return (
+    <div className="bg-accent flex items-center justify-center p-[1.875rem] mt-5">
+      <p className="px-08 py-04 text-[0.625rem] opacity-75">
+        © 2023 Copyright
+        <span className="font-semibold"> FSW</span> Store
+      </p>
+    </div>
+  );
+};
