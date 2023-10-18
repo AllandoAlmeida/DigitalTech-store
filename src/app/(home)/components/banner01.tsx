63648983f061666d0538051411990092ca9c6ab4
AllandoAlmeida <alandoalmeida0803@7j50z3.onmicrosoft.com>
2023-10-18 15:34:42 -0300

    feat: add ProductItems and ProductList

--- src/app/(home)/components/banner01.tsx
+++ src/app/(home)/components/banner01.tsx
@@ -1,14 +1,16 @@
 import Image from "next/image";
 
 export const BannerHm01 = () => {
-    return ( 
-        <Image
+  return (
+    <div className="mt-5 px-5">
+      <Image
         src="/banner-home-01.png"
         height={0}
         width={0}
-        className="h-auto w-full"
+        className="h-auto w-full "
         sizes="100vw"
         alt="até 50% de desconto esse mês!"
       />
-     );
-}
+    </div>
+  );
+};
--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,14 +1,24 @@
-"use client";
-
+import { prismaClient } from "@/lib/prisma";
 import { BannerHm01 } from "./components/banner01";
 import { Categories } from "./components/catogories";
+import ProductList from "./components/product-list";
 
-export default function Home() {
+export default async function Home() {
+  const deals = await prismaClient.product.findMany({
+    where: {
+      discountPercentage: {
+        gt: 0,
+      },
+    },
+  });
   return (
-    <div className="p-5">
+    <div className="">
       <BannerHm01 />
-      <div className="mt-8">
+      <div className="mt-8 px-5">
         <Categories />
+      </div>
+      <div className="mt-8 px-5">
+        <ProductList products={deals}/>
       </div>
     </div>
   );
