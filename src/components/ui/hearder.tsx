c164737b7fd49f7d1253e2792588645cacd48490
AllandoAlmeida <alandoalmeida0803@7j50z3.onmicrosoft.com>
2023-10-18 22:03:43 -0300

    feat: ensure user goes to catalog page on catalog button click

--- src/components/ui/hearder.tsx
+++ src/components/ui/hearder.tsx
@@ -10,10 +10,17 @@ import {
 } from "lucide-react";
 import { Card } from "./card";
 import { Button } from "./button";
-import { Sheet, SheetContent, SheetHeader, SheetTrigger } from "./sheet";
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTrigger,
+  SheetClose,
+} from "./sheet";
 import { signIn, signOut, useSession } from "next-auth/react";
 import { Avatar, AvatarFallback, AvatarImage } from "@radix-ui/react-avatar";
 import { Separator } from "@radix-ui/react-separator";
+import Link from "next/link";
 
 const Header = () => {
   const { status, data } = useSession();
@@ -85,10 +92,17 @@ const Header = () => {
               <PercentIcon size={16} />
               Ofertas
             </Button>
-            <Button variant={"outline"} className="w-full justify-start gap-2">
-              <ListOrderedIcon size={16} />
-              Catálogo
-            </Button>
+            <SheetClose asChild>
+              <Link href={"/catalog"}>
+                <Button
+                  variant={"outline"}
+                  className="w-full justify-start gap-2"
+                >
+                  <ListOrderedIcon size={16} />
+                  Catálogo
+                </Button>
+              </Link>
+            </SheetClose>
           </div>
         </SheetContent>
       </Sheet>
