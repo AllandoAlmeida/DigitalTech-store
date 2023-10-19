dca94e85b1a2f4b1505a08513c20482117e28b3d
AllandoAlmeida <alandoalmeida0803@7j50z3.onmicrosoft.com>
2023-10-19 09:59:59 -0300

    feat: add Product Imagens product components

---
+++ src/app/product/[slug]/components/product-images.tsx
@@ -0,0 +1,59 @@
+"use client";
+
+import Image from "next/image";
+import { useState } from "react";
+
+interface ProductImagesProps {
+  name: string;
+  imageUrls: string[];
+}
+
+const ProductImages = ({ name, imageUrls }: ProductImagesProps) => {
+  const [currentImage, setCurrentImage] = useState(imageUrls[0]);
+
+  const handleImageClick = (imageUrls: string) => {
+    setCurrentImage(imageUrls);
+  };
+
+  return (
+    <div className="flex flex-col">
+      <div className="flex h-[380px] w-full items-center justify-center bg-accent">
+        <Image
+          src={currentImage}
+          alt={name}
+          width={0}
+          height={0}
+          sizes="100vw"
+          className="h-auto max-h-[70%] w-auto max-w-[80%]"
+          style={{
+            objectFit: "contain",
+          }}
+        />
+      </div>
+
+      {/* Botões com a seguencia de imagens */}
+
+      <div className="mt-8 grid grid-cols-4 gap-4 px-5">
+        {imageUrls.map((imageUrl) => (
+          <button
+            key={imageUrl}
+            className={`flex h-[100px] items-center justify-center rounded-lg bg-accent
+          ${imageUrl === currentImage && "border-2 border-primary"}`}
+          >
+            <Image
+              src={imageUrl}
+              alt={name}
+              width={0}
+              height={0}
+              sizes="100vw"
+              className="h-auto max-h-[70%] w-auto max-w-[80%]"
+              onClick={() => handleImageClick(imageUrl)}
+            />
+          </button>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ProductImages;
